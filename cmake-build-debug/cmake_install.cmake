93e389afd7d4f241a8909bb360206cb09032fa2f
Arsenoks4132 <Danov2005@yandex.ru>
2023-11-20 13:06:29 +0300

    Laptop

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Danov/CLionProjects/bacteria
+# Install script for directory: C:/Users/Danov/CLionProjects/Bacteria
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Danov/CLionProjects/bacteria/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Danov/CLionProjects/Bacteria/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
